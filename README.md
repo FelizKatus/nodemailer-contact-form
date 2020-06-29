04fca6cbfd04437ed180725c23954873c1ba207d
felizkatus <felizkatus@gmail.com>
2020-06-29 13:51:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Tailwind/Nodemailer Contact Form
+
+Simple formulario de contactos con protección del SPAM. Se desarrolla con las siguientes fantasticas tecnologías:
+
+* Node/Express
+* Nodemailer
+* Handlebars
+* Tailwind
+
+#### De la caja
+
+* Protección del SPAM
+
+#### Como arrancar
+
+<code>yarn</code>
+<code>yarn run app</code>
+
+Para recibir los correos a su cuenta de Google hay que permitir el acceso de aplicaciones poco seguras:
+
+<a href="https://myaccount.google.com/lesssecureapps" target="_blank">
+  https://myaccount.google.com/lesssecureapps
+</a>
+
+#### Licensia:
+
+Eres libre y tienes derecho de hacer todo lo que quieres.
+
+#### Pantallas:
+
+Formulario de contacto ->
+![Formulario de contacto](screenshots/contact.png "Formulario de contacto")
---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer')
+const handlebars = require('express-handlebars').create({ defaultLayout: 'main' })
+const credentials = require('./credentials')
+
+require('dotenv').config()
+
+const app = express()
+
+// Environment
+
+const PORT = process.env.PORT || 8080
+
+// Handlebars
+
+app.engine('handlebars', handlebars.engine)
+app.set('view engine', 'handlebars')
+
+// Static
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// URLencode
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Routing
+
+app.get('/', (req, res) => {
+  res.render('contact', { layout: null })
+})
+
+app.post('/', (req, res) => {
+  const mailTransport = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: credentials.gmail.user,
+      pass: credentials.gmail.password
+    }
+  })
+
+  const mailOptions = {
+    from: 'Contact Form <do-not-reply@gmail.com>',
+    to: 'felizkatus@gmail.com',
+    subject: 'Contact Form Submission',
+    html:
+      '<p>You have submission the following details...</p><p><strong>Name:</strong> ' + req.body.name +
+      '</p><p><strong>Email:</strong> ' + req.body.email +
+      '</p><p><strong>Message:</strong> ' + req.body.message + '</p>',
+    generateTextFromHtml: true
+  }
+
+  if (req.body.human) {
+    console.log('true')
+
+    mailTransport.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log('Message could not be sent: ' + error)
+        res.redirect('/')
+      } else {
+        console.log('Message sent: ' + info.response)
+        res.redirect('/')
+      }
+    })
+  } else {
+    console.log('false')
+    res.redirect('/')
+  }
+})
+
+app.listen(PORT)
